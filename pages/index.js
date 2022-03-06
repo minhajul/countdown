4718ba94537e45be4e9ef00ed7783e76f5b8d947
minhajul <kmsohelinfo@gmail.com>
2022-03-06 19:35:33 +0600

    Updated

--- pages/index.js
+++ pages/index.js
@@ -38,30 +38,34 @@ export default function Home() {
 
     return (
         <div className="bg-gray-900">
-            <main className="max-w-full mx-auto min-h-screen bg-gray-900 opacity-70">
-                <div className="flex flex-col justify-center items-center absolute inset-40 ">
-                    {timerComponents.length ?
-                        <div className="grid grid-cols-4 gap-6">
-                            <div className="col-span-4 lg:col-span-1 text-center bg-blue-400 p-5 rounded-md shadow-md">
-                                <span className="text-7xl">{ timeLeft['days'] }</span>
-                                <span className="block text-sm">Days</span>
-                            </div>
-                            <div className="col-span-4 lg:col-span-1 text-center bg-blue-400 p-5 rounded-md shadow-md">
-                                <span className="text-7xl">{ timeLeft['hours'] }</span>
-                                <span className="block text-sm">Hours</span>
-                            </div>
-                            <div className="col-span-4 lg:col-span-1 text-center bg-blue-400 p-5 rounded-md shadow-md">
-                                <span className="text-7xl">{ timeLeft['minutes'] }</span>
-                                <span className="block text-sm">Minutes</span>
-                            </div>
-                            <div className="col-span-4 lg:col-span-1 text-center bg-blue-400 p-5 rounded-md shadow-md">
-                                <span className="text-7xl">{ timeLeft['seconds'] }</span>
-                                <span className="block text-sm">Seconds</span>
-                            </div>
+            <main className="relative max-w-4xl mx-auto px-10 py-20 min-h-screen flex flex-col justify-center">
+                {timerComponents.length ?
+                    <div className="grid grid-cols-4 gap-6">
+                        <div className="col-span-4 lg:col-span-1 text-center bg-blue-400 p-10 rounded-md shadow-md">
+                            <span className="text-7xl">{ timeLeft['days'] }</span>
+                            <span className="block text-sm">Days</span>
                         </div>
-                   :
-                        <h2 className="text-white text-4xl italic">Time is up! Boss is here.</h2>}
-                </div>
+
+                        <div className="col-span-4 lg:col-span-1 text-center bg-blue-400 p-10 rounded-md shadow-md">
+                            <span className="text-7xl">{ timeLeft['hours'] }</span>
+                            <span className="block text-sm">Hours</span>
+                        </div>
+
+                        <div className="col-span-4 lg:col-span-1 text-center bg-blue-400 p-10 rounded-md shadow-md">
+                            <span className="text-7xl">{ timeLeft['minutes'] }</span>
+                            <span className="block text-sm">Minutes</span>
+                        </div>
+
+                        <div className="col-span-4 lg:col-span-1 text-center bg-blue-400 p-10 rounded-md shadow-md">
+                            <span className="text-7xl">{ timeLeft['seconds'] }</span>
+                            <span className="block text-sm">Seconds</span>
+                        </div>
+                    </div>
+               : <h2 className="text-white text-4xl italic">Time is up! Boss is here.</h2>}
+
+                <p className="absolute bottom-0 text-white text-xs pt-3 pb-5">
+                    Copyright @2022 <a href="https://minhajul.vercel.com/">MD Minhajul Islam</a>. All rights reserved.
+                </p>
             </main>
         </div>
     )
